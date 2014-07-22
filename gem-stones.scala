928c752cb174c8191be05800b54b8853156e58cc
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2014-07-22 16:24:57 +0100

    unfinished solution to gem stones problem

---
+++ gem-stones.scala
@@ -0,0 +1,13 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        // read first integer n, which will be our test. i.e. gem must appear n times
+        // loop through the next lines of input
+        // create a set from line
+        // add a count for each gem encountered
+        // count the number of gem which was encountered n times
+        var n = readInt()
+        var line = readLine()
+        println(line)
+    }
+}
